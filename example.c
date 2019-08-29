e4607dc353e2ea1b8c68e7e1b827dae7a377d39e
MistFuror <acastefanovic99@hotmail.com>
2019-08-29 23:14:26 +0200

    Removed constants used for testing, changed example

--- example.c
+++ example.c
@@ -1,13 +1,12 @@
 #include "symee.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 int main(int argc, char *argv[])
 {
 	expressionInfo expressionState;
 	double res;
-	expressionState = evaluateExpression(argv[1], &res);
+	expressionState = evaluateExpression("123.5 - 32pi + 2sin(pi/2) + max(phi, avg(1, e))", &res);
 	if(expressionState.status == VALID)
 		printf("%lf\n", res);
 	else
